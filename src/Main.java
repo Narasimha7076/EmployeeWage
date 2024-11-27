626a60e152b2a72448e25d8899f975f341fda3a5
Narasimha7076 <bablusomesula@gmail.com>
2024-11-27 18:22:56 +0530

    Added: Feature attedance

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-
+//        System.out.println("Hello, World!");
+        Employee e1 = new Employee("Ram",123);
+        Random r = new Random();
+        int i = (r.nextInt(1000))%2;
+        System.out.println(i);
+        switch (i){
+            case 1-> e1.attandance(true);
+            default-> e1.attandance(false);
+        }
     }
 }
